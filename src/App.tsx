5dd862ae637d98cffa871136583f22932b533732
Yaroslav <bogdan.az62@gmail.com>
2024-09-04 22:34:35 +0300

    created functions checkColor and needChangeColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState, FC } from 'react'
+import { useState, FC  } from 'react'
 
 import './App.css'
 import { Button } from './components/button';
 
 const App: FC = () => {
-  const [buttonColors, setButtonColors] = useState<string[]>(['black','black','black','black','black','black','black','black','black','black','black','black']);
+  const [buttonColors, setButtonColors] = useState<string[]>(['green', 'red', 'blue']);
 
-  const [buttonStates, setButtonStates] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [buttonStates, setButtonStates] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   
   const getElementFromArray = (array: number[] | string[]): number | string => {
     const randomIndex = Math.trunc(Math.random() * array.length)
 
     return array[randomIndex];
   }
+  
 
   const getState = (): number => {
     const states: number[] = [-1, 0, 1]
@@ -21,13 +22,12 @@ const App: FC = () => {
   }
   
   const getColor = (): string => {
-    const colors: string[] = ['navy', 'darkgreen', 'darkred', 'purple', 'brown']
+    const colors: string[] = ['green', 'red', 'blue']
 
     return getElementFromArray(colors) as string
   }
 
   const clickButton = (buttonIndex: number) => {
-
 
     const updatedStates = buttonStates.map((state, index) => {
       if (buttonIndex === index) {
@@ -40,22 +40,35 @@ const App: FC = () => {
       if (buttonIndex === index) {
         return getColor();
       }
+    
       return color;
+    
     })
 
-    // const updatedColors = [...buttonColors];
-    // updatedColors[buttonIndex] = getColor();
-    
     setButtonColors(updatedColors)
     setButtonStates(updatedStates)
 
+    checkColor(buttonColors[buttonIndex])
+
   }
 
-  const renderButton = (index: number) => <Button onClick={() => clickButton(index)} color={buttonColors[index]} state={buttonStates[index]} />;
+  const needChangeColor = (color: string, num = 2): boolean => checkColor(color) > num
+    
+  const checkColor = (color: string) => buttonColors.reduce((acc: number, item: string) => {
+    if (color === item) {
+      return acc + 1 
+    }
+    return acc
+  }, 0) 
+
+
+
+
+  const renderButton = (index: number) => <Button onClick={ () => clickButton(index)} color={buttonColors[index]} />;
 
   return (
     <>
-      <div className="card">
+      <div className="card" >
         <div>
           {buttonColors.map((_, index) => renderButton(index) )}
         </div>
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,29 +1,23 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+
 import { FC } from "react";
+// import FullLikeIcon from '../../assets/Icons/FullLikeIcon.svg?react'
+// import LikeIcon from '../../assets/Icons/LikeIcon.svg?react'
+
 
 interface ButtonProps {
     color: string;
     onClick: () => void;
-    state: number;
 }
 
-export const Button: FC<ButtonProps> = ({color, onClick, state}) => {
+export const Button: FC<ButtonProps> = ({color, onClick}) => {
 
-    const renderLetter = () => {
-
-        switch (state) {
-            case -1:
-                return 'X'
-            case 1:
-                return 'O'
-            default:
-                return
-        }
-    }
     return (
     <button onClick={onClick} style={{    
         backgroundColor: color
     }} >
-        {renderLetter()}
+
     </button>   
     )
 }
